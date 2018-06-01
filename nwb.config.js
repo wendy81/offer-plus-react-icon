c863b1afe1554d3df616fff1cb234c94f2a15355
Alexander Berezovsky <fantik217@gmail.com>
2018-06-01 08:48:49 +0700

    feature/update-1, babel plugin react-inline-svg in nwb.config

--- nwb.config.js
+++ nwb.config.js
@@ -3,5 +3,25 @@ module.exports = {
   npm: {
     esModules: true,
     umd: false
+  },
+  babel: {
+    "plugins": [
+      "transform-class-properties",
+      "transform-es2015-destructuring",
+      "transform-object-rest-spread",
+      "transform-inline-environment-variables",
+      "transform-es2015-modules-commonjs",
+      [
+        "styled-components",
+        {
+          "ssr": false,
+          "minify": false,
+          "displayName": true,
+          "preprocess": false,
+          "transpileTemplateLiterals": false
+        }
+      ],
+      "inline-react-svg"
+    ]
   }
 }
