ae802525ec84ba738b76684c16e6527ca7f3914f
Shadwar <vladoivan@mail.ru>
2018-06-09 11:33:16 +0300

    feature/add-arrow-icons, icons and demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src/index'
+import Icon from '../../src/index';
+import { ICONS } from '../../src/svg';
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>react-icon Demo</h1>
-      <Example/>
+      {Object.keys(ICONS).map(type => (
+        <div>
+          <Icon type={type} size={16} style={{fill: "#000"}}/>
+          {" - "}{type}
+        </div>
+      ))}
     </div>
   }
 }
