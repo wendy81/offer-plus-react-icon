b2d3f1e9d7a19ebce607ed8a484b8f9a09643d5f
Alexander Berezovsky <fantik217@gmail.com>
2018-06-10 17:13:43 +0700

    feature/update-1, turnover, write-off-code

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,110 +1,114 @@
 import React from "react";
 
-import AddBlank from './add-blank.svg';
-import Aggregated from './aggregated.svg';
-import ArrowsFromCenter from './arrows-from-center.svg';
-import ArrowsToCenter from './arrows-to-center.svg';
-import BoxOnLoader from './box-on-loader.svg';
-import Cancel from './cancel.svg';
-import Cart from './cart.svg';
-import CheckBso from './check-bso.svg';
-import CheckBsoReturn from './check-bso-return.svg';
-import Checking from './checking.svg';
-import Checkmark from './checkmark.svg';
-import Cigarettes from './cigarettes.svg';
-import ClosedArrow from './closed-arrow.svg';
-import CodeInvalid from './code-invalid.svg';
-import CrossHumanIcon from './cross-human-icon.svg';
-import Disaggregated from './disaggregated.svg';
-import DocAggregation from './doc-aggregation.svg';
-import DocDisaggregation from './doc-disaggregation.svg';
-import EditBlank from './edit-blank.svg';
-import EditFilledBlank from './edit-filled-blank.svg';
-import Emission from './emission.svg';
-import Emitted from './emitted.svg';
-import ErrorForm from './error-form.svg';
-import FilledBlank from './filled-blank.svg';
-import FilledCart from './filled-cart.svg';
-import Formed from './formed.svg';
-import GroupBlock from './group-block.svg';
-import HumanIcon from './human-icon.svg';
-import NotAllow from './not-allow.svg';
-import Pan from './pan.svg';
-import PrintFlame from './print-flame.svg';
-import Star from './star.svg';
-import Truck from './truck.svg';
-import Filter from './filter.svg';
-import Notification from './notification.svg';
-import Edit from './edit.svg';
-import LogoCRPT from './logo-crpt.svg';
-import Download from './download.svg';
-import Search from './search.svg';
-import SoldOut from './sold-out.svg';
-import Phone from './phone.svg';
-import Mail from './mail.svg';
-import Pack from './pack.svg';
-import Block from './block.svg';
-import Box from './box.svg';
-import Utd from './utd.svg';
-import Agregation from './agregation.svg';
-import Issued from './issued.svg';
-import DocError from './doc-error.svg';
-import Member from './member.svg';
-import NotMember from './not-member.svg';
-import ChatSendArrow from './chat-send-arrow.svg';
+import AddBlank from "./add-blank.svg";
+import Aggregated from "./aggregated.svg";
+import ArrowsFromCenter from "./arrows-from-center.svg";
+import ArrowsToCenter from "./arrows-to-center.svg";
+import BoxOnLoader from "./box-on-loader.svg";
+import Cancel from "./cancel.svg";
+import Cart from "./cart.svg";
+import CheckBso from "./check-bso.svg";
+import CheckBsoReturn from "./check-bso-return.svg";
+import Checking from "./checking.svg";
+import Checkmark from "./checkmark.svg";
+import Cigarettes from "./cigarettes.svg";
+import ClosedArrow from "./closed-arrow.svg";
+import CodeInvalid from "./code-invalid.svg";
+import CrossHumanIcon from "./cross-human-icon.svg";
+import Disaggregated from "./disaggregated.svg";
+import DocAggregation from "./doc-aggregation.svg";
+import DocDisaggregation from "./doc-disaggregation.svg";
+import EditBlank from "./edit-blank.svg";
+import EditFilledBlank from "./edit-filled-blank.svg";
+import Emission from "./emission.svg";
+import Emitted from "./emitted.svg";
+import ErrorForm from "./error-form.svg";
+import FilledBlank from "./filled-blank.svg";
+import FilledCart from "./filled-cart.svg";
+import Formed from "./formed.svg";
+import GroupBlock from "./group-block.svg";
+import HumanIcon from "./human-icon.svg";
+import NotAllow from "./not-allow.svg";
+import Pan from "./pan.svg";
+import PrintFlame from "./print-flame.svg";
+import Star from "./star.svg";
+import Truck from "./truck.svg";
+import Filter from "./filter.svg";
+import Notification from "./notification.svg";
+import Edit from "./edit.svg";
+import LogoCRPT from "./logo-crpt.svg";
+import Download from "./download.svg";
+import Search from "./search.svg";
+import SoldOut from "./sold-out.svg";
+import Phone from "./phone.svg";
+import Mail from "./mail.svg";
+import Pack from "./pack.svg";
+import Block from "./block.svg";
+import Box from "./box.svg";
+import Utd from "./utd.svg";
+import Agregation from "./agregation.svg";
+import Issued from "./issued.svg";
+import DocError from "./doc-error.svg";
+import Member from "./member.svg";
+import NotMember from "./not-member.svg";
+import ChatSendArrow from "./chat-send-arrow.svg";
+import Turnover from "./turnover.svg";
+import WriteOffCode from "./write-off-code.svg";
 
 const ICONS = {
-  'add-blank': props => <AddBlank {...props} />,
+  "add-blank": props => <AddBlank {...props} />,
   aggregated: props => <Aggregated {...props} />,
-  'arrows-from-center': props => <ArrowsFromCenter {...props} />,
-  'arrows-to-center': props => <ArrowsToCenter {...props} />,
-  'box-on-loader': props => <BoxOnLoader {...props} />,
+  "arrows-from-center": props => <ArrowsFromCenter {...props} />,
+  "arrows-to-center": props => <ArrowsToCenter {...props} />,
+  "box-on-loader": props => <BoxOnLoader {...props} />,
   cancel: props => <Cancel {...props} />,
   cart: props => <Cart {...props} />,
-  'check-bso': props => <CheckBso {...props} />,
-  'check-bso-return': props => <CheckBsoReturn {...props} />,
+  "check-bso": props => <CheckBso {...props} />,
+  "check-bso-return": props => <CheckBsoReturn {...props} />,
   checking: props => <Checking {...props} />,
   checkmark: props => <Checkmark {...props} />,
   cigarettes: props => <Cigarettes {...props} />,
-  'closed-arrow': props => <ClosedArrow {...props} />,
-  'code-invalid': props => <CodeInvalid {...props} />,
-  'cross-human-icon': props => <CrossHumanIcon {...props} />,
+  "closed-arrow": props => <ClosedArrow {...props} />,
+  "code-invalid": props => <CodeInvalid {...props} />,
+  "cross-human-icon": props => <CrossHumanIcon {...props} />,
   disaggregated: props => <Disaggregated {...props} />,
   download: props => <Download {...props} />,
-  'doc-aggregation': props => <DocAggregation {...props} />,
-  'doc-disaggregation': props => <DocDisaggregation {...props} />,
-  'edit-blank': props => <EditBlank {...props} />,
-  'edit-filled-blank': props => <EditFilledBlank {...props} />,
+  "doc-aggregation": props => <DocAggregation {...props} />,
+  "doc-disaggregation": props => <DocDisaggregation {...props} />,
+  "edit-blank": props => <EditBlank {...props} />,
+  "edit-filled-blank": props => <EditFilledBlank {...props} />,
   emission: props => <Emission {...props} />,
   emitted: props => <Emitted {...props} />,
-  'error-form': props => <ErrorForm {...props} />,
-  'filled-blank': props => <FilledBlank {...props} />,
-  'filled-cart': props => <FilledCart {...props} />,
+  "error-form": props => <ErrorForm {...props} />,
+  "filled-blank": props => <FilledBlank {...props} />,
+  "filled-cart": props => <FilledCart {...props} />,
   formed: props => <Formed {...props} />,
-  'group-block': props => <GroupBlock {...props} />,
-  'human-icon': props => <HumanIcon {...props} />,
-  'not-allow': props => <NotAllow {...props} />,
+  "group-block": props => <GroupBlock {...props} />,
+  "human-icon": props => <HumanIcon {...props} />,
+  "not-allow": props => <NotAllow {...props} />,
   pan: props => <Pan {...props} />,
-  'print-flame': props => <PrintFlame {...props} />,
+  "print-flame": props => <PrintFlame {...props} />,
   star: props => <Star {...props} />,
   truck: props => <Truck {...props} />,
   filter: props => <Filter {...props} />,
   notification: props => <Notification {...props} />,
   edit: props => <Edit {...props} />,
-  'logo-crpt': props => <LogoCRPT {...props} />,
+  "logo-crpt": props => <LogoCRPT {...props} />,
   search: props => <Search {...props} />,
-  'sold-out': props => <SoldOut {...props} />,
-  'phone': props => <Phone {...props} />,
-  'mail': props => <Mail {...props} />,
+  "sold-out": props => <SoldOut {...props} />,
+  phone: props => <Phone {...props} />,
+  mail: props => <Mail {...props} />,
   pack: props => <Pack {...props} />,
   block: props => <Block {...props} />,
   box: props => <Box {...props} />,
   utd: props => <Utd {...props} />,
   agregation: props => <Agregation {...props} />,
   issued: props => <Issued {...props} />,
-  'doc-error': props => <DocError {...props} />,
+  "doc-error": props => <DocError {...props} />,
   member: props => <Member {...props} />,
-  'not-member': props => <NotMember {...props} />,
-  'chat-send-arrow': props => <ChatSendArrow {...props} />,
+  "not-member": props => <NotMember {...props} />,
+  "chat-send-arrow": props => <ChatSendArrow {...props} />,
+  turnover: props => <Turnover {...props} />,
+  'write-off-code': props => <WriteOffCode {...props} />
 };
 export { ICONS };
